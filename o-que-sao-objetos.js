59cf040b1e0e477b9f99bb16a0c40cb9bf0a19d8
Alex Boccia <alexboccia@MacBook-Pro-de-Alex.local>
2021-09-11 00:24:41 -0300

    o que sao objetos

---
+++ o-que-sao-objetos.js
@@ -0,0 +1,12 @@
+// objeto é uma estrutura de dados que permite organizar informações de uma abstração do mundo real, 
+// por exemplo: uma pessoa, um carro, uma conta corrente, etc... que permite armazenar diferentes 
+// tipos de primitivos: string, number, boolean, relacionados através de chaves e valores.
+// Abaixo um exemplo de abstração de uma pessoa (cliente) para um objeto JavaScript:
+
+const cliente = {
+    nome : "Alex",
+    sobrenome : "Boccia",
+    idade : 41,
+    cpf : 12312312345,
+    email: "alexboccia@gmail.com"
+}
